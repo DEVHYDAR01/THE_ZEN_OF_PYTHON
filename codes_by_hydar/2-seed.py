20533f2c91c6973e51e9e44a100278f03c7a4f27
DEVHYDAR01 <aliyuahmaad60@gmail.com>
2024-02-20 15:24:05 +0100

    This commit will push task 2

--- codes_by_hydar/2-seed.py
+++ codes_by_hydar/2-seed.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
+import random
 
+test_seed = int(input("enter your seed: "))
+random.seed(test_seed)
+
+seed_HT = random.randint(0, 1)
+if seed_HT == 0:
+    print("Heads")
+elif seed_HT == 1:
+    print("Tails")
+else:
+    print("enter some value")
